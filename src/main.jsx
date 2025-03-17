ac7c0adfcea569c442c92ff3cc93a1815ba7874b
Rajan Ladwa <rladwa@gmail.com>
2025-03-17 15:41:21 +0900

    first component created

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,17 @@
 import './index.css';
+
+// createRoot = function that creates a root for rendering React components into the DOM
+import { createRoot } from 'react-dom/client';
+
+console.log("connected");
+
+const root = createRoot(document.getElementById('root'));
+
+
+// Basic React component
+function Hello() {
+  return <h1>Hello, world!</h1>;
+}
+
+// Render the component as a HTML element
+root.render(<Hello />);
