30299ce7ec8d0acba92012071085dc3087a7f058
Rajan Ladwa <rladwa@gmail.com>
2025-03-19 15:03:44 +0900

    added search functionality on keyup, effect for initial cafes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import CafeList from './components/CafeList/CafeList';
 import SideBar from './components/Sidebar/Sidebar';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 // Flow
 // 1. Create a new component called App
@@ -16,57 +17,29 @@ const App = () => {
 // cafeData = array of objects
 // setCafeData = function to update the cafeData state
 // useState = hook to create a state variable
-const [cafeData, setCafeData] = useState([
-  {
-      "picture": "https://laptopfriendly.co/images/places/tokyo/ddsk-saigon-kitchen/ddsk-saigon-kitchen--tokyo.jpg",
-      "title": "DDSK SAIGON KITCHEN",
-      "address": "ss, 135-0061, Tokyo",
-      "criteria": []
-  },
-  {
-      "picture": "https://laptopfriendly.co/images/places/tokyo/common/common--tokyo.jpg",
-      "title": "COMMON",
-      "address": "106-0032, Tokyo",
-      "criteria": [
-          "Stable Wi-Fi",
-          "Power sockets",
-          "Quiet",
-          "Coffee",
-          "Food"
-      ]
-  },
-  {
-      "picture": "https://laptopfriendly.co/images/places/tokyo/brooklyn-parlor-shinjuku/brooklyn-parlor-shinjuku%20shinjuku-city%20brooklyn-parlor-shinjuku-shinjuku-city%20.jpg",
-      "title": "BROOKLYN PARLOR SHINJUKU",
-      "address": "Shinjuku Marui Annex B1F, 3-1-26 Shinjuku, Shinjuku-ku, Tokyo, 160-0022, Tokyo",
-      "criteria": [
-          "Stable Wi-Fi",
-          "Power sockets",
-          "Quiet",
-          "Coffee",
-          "Food"
-      ]
-  }
-]
-);
+const [cafeData, setCafeData] = useState([]);
 
-  const getCafes = () => {
-    const url = 'https://matcha-and-keyboard-f549965e60e7.herokuapp.com/api/v1/cafes';
-    fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-        // set cafeData to the data from the API
-        setCafeData(data);
-        console.log(cafeData);
-      })
-      .catch(error => console.log(error));
-  };
+const getCafes = () => {
+  const url = `https://matcha-and-keyboard-f549965e60e7.herokuapp.com/api/v1/cafes`;
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      // set cafeData to the data from the API
+      setCafeData(data);
+    });
+};
+
+// useEffect = hook to run code when the component is rendered
+// getCafes = function to fetch the cafe data from the API
+// [] = empty array to run the code only once
+useEffect(() => {
+  getCafes();
+}, []);
 
   return (
     <div className="app-frame">
-      <Navbar />
-      <button onClick={getCafes} >See All Cafes! ☕</button>
+      <Navbar setCafeData={setCafeData} />
         <div className="app-body">
           <SideBar />
           <CafeList cafeData={cafeData}/>
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,12 +3,25 @@
 import React from 'react';
 import './Searchbar.css';
 
-function Searchbar() {
+function Searchbar({ setCafeData }) {
+
+  const getCafes = (event) => {
+    const query = event.target.value;
+    const url = `https://matcha-and-keyboard-f549965e60e7.herokuapp.com/api/v1/cafes?title=${query}`;
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        // set cafeData to the data from the API
+        setCafeData(data);
+      })
+      .catch(error => console.log(error));
+  };
 
   return (
     <div className="searchbar input-group">
       <span className="input-group-text"><i className="fa-solid fa-magnifying-glass"></i></span>
-      <input placeholder="Starbucks" type="text" className="form-control" />
+      <input onChange={getCafes} placeholder="Search for a cafe..." type="text" className="form-control" />
     </div>
   );
 }
